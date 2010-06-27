ce766140b133f66af84eba42875a395dce525b3d
Jake Paul <me@jakepaul.com>
2010-06-27 15:58:12 -0400

    Version bump, fixed a bug in the permalink replacement

--- Simplr.safariextension/tumblr.js
+++ Simplr.safariextension/tumblr.js
@@ -2,23 +2,22 @@ jQuery.noConflict();
 
 jQuery(document).ready(function(){
   
+  jQuery('li.post.new_post + li.post.same_user_as_last').removeClass('same_user_as_last');
+  
   var path = window.location.pathname;
   
-  if (path.match(/^\/dashboard$/)) {
-    jQuery('.permalink').each(function(){
-      url = jQuery(this).parent().attr('href');
-      post = jQuery(this).parents('li.post');
-      info = post.find('.post_info')
-      if (info.length) {
-        info.children('a:first').attr('href', url)
-      } else {
-        user = post.prevAll(':not(.same_user_as_last):first .post_info a:first').html();
-        post.find('.post_controls').after('<div class="post_info"><a href="'+url+'">'+user+'</a>:</div>');
-      }
-    });
+  if (path.match(/^\/dashboard/)) {
+    permalink_replace();
+    
+    // Uncomment to loop the permalink replace function so that it works with endless scrolling.
+    // Obviously, this will have a measurable impact on performance, which is why it's not enabled by default.
+    // It's also quite a hack: there's probably a much better way to do this, but I don't use endless 
+    // scrolling so I'm not going to figure it out.
+    
+    // if (jQuery('#auto_pagination_loader').length != 0) {
+    //   loop_permalink_replace();
+    // }
   }
-  
-  jQuery('li.post.new_post + li.post.same_user_as_last').removeClass('same_user_as_last');
   
   if (path.match(/^\/tumblelog\/([^\/]+)$/)||path.match(/^\/dashboard$/)) {
     if (path.match(/^\/dashboard$/)) { path = ''; }
@@ -49,3 +48,22 @@ jQuery(document).ready(function(){
   });
     
 });
+
+function permalink_replace() {
+  jQuery('.permalink').each(function(){
+    url = jQuery(this).parent().attr('href');
+    post = jQuery(this).parents('li.post');
+    info = post.find('.post_info')
+    if (info.length) {
+      info.children('a:first').attr('href', url)
+    } else {
+      user = post.prevAll(':not(.same_user_as_last):first .post_info a:first').html();
+      post.find('.post_controls').after('<div class="post_info"><a href="'+url+'">'+user+'</a>:</div>');
+    }
+  });
+}
+
+function loop_permalink_replace() {
+  permalink_replace();
+  setTimeout(loop_permalink_replace, 5000);
+}
